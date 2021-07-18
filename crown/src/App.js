07769e36624536f0dc247ae47bd193f3618d4150
Andrej Kasapinov <72466548+kasapinovandrej@users.noreply.github.com>
2021-07-18 18:29:06 +0200

    adding header component

--- crown/src/App.js
+++ crown/src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import HomePage from './pages/homepage/homepage.component';
-import { Route, Switch } from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import { Route, Switch } from 'react-router-dom'
 
 
 function App() {
   return (
     <div >
+      <Header />
       <Switch>
         <Route path='/' exact component={HomePage} />
         <Route path='/shop' component={ShopPage} />
