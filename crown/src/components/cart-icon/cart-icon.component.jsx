0dbe8040c78878a5c521df93cd163a6357cb6f7e
Andrej Kasapinov <72466548+kasapinovandrej@users.noreply.github.com>
2021-07-24 12:20:50 +0200

    installed reselect

--- crown/src/components/cart-icon/cart-icon.component.jsx
+++ crown/src/components/cart-icon/cart-icon.component.jsx
@@ -3,11 +3,13 @@ import "./cart-icon.styles.scss";
 import { ReactComponent as ShopingIcon } from "../../assets/shopping-bag.svg";
 import { connect } from "react-redux";
 import { toggleCart } from "../../redux/cart/cart.actions";
-const CartIcon = ({ toggleCart }) => {
+import { selectCartItemsCount } from "../../redux/cart/cart.selector";
+
+const CartIcon = ({ toggleCart, itemCount }) => {
   return (
     <div className="cart-icon" onClick={toggleCart}>
       <ShopingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 };
@@ -16,4 +18,8 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCart: () => dispatch(toggleCart()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
